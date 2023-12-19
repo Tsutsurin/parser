dc7895572c74924e5068a7e7226ea5be6e16fcc6
Nikita <86563787+Tsutsurin@users.noreply.github.com>
2023-12-19 13:17:00 +0300

    Update rss.py

--- rss.py
+++ rss.py
@@ -3,6 +3,16 @@ import time
 from datetime import datetime
 import pandas as pd
 import re
+import feedparser
+from urllib.parse import urlparse
+
+
+def is_valid_url(url):
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
 
 
 def main():
@@ -14,59 +24,71 @@ def main():
     with open('utilities/rss.txt', 'r') as f:
         lines = [line.rstrip() for line in f]
 
-    counter = 1
-    source = 'NEWS'
+    counter = 0
     df = pd.DataFrame(
-        {'№': [''], 'Источник': [''], 'Дата публикации': [''], 'CVE': [''], 'Ссылки': ['']})
+        {'№': [''], 'Источник': [''], 'Дата публикации': [''], 'CVE': [''], 'Заголовок': [''], 'Ссылки': ['']})
 
     for url in lines:
-        driver.get(url)
-        time.sleep(2)
-        soup = functions.get_soup(driver, url, None)
-
-        if soup.find('pubdate'):
-            items = soup.find_all('item')
-            for item in items:
-                pubdate = item.find('pubdate').text
-                try:
-                    date_object = datetime.strptime(pubdate, '%a, %d %b %Y %H:%M:%S %z').date()
-                except ValueError:
-                    date_object = datetime.strptime(pubdate, '%a, %d %b %Y %H:%M:%S %Z').date()
-
-                formatted_date = date_object.strftime('%d.%m.%Y')
-
-                if date1 <= date_object:
-                    link_tg = item.find("link")
-                    link = link_tg.next_sibling
-                    print(f'Идет поиск CVE в {link}')
-
-                    driver.get(link)
-                    soup = functions.get_soup(driver, link, None)
-
-                    cve_pattern = re.compile(r'CVE-\d{4}-\d{4,7}')
-                    cve_matches = soup.find_all(string=cve_pattern)
-
-                    if cve_matches:
-                        for match in cve_matches:
-                            cve = re.findall(cve_pattern, match)
-                            str_cve = ' '.join(set(cve))
-
-                        df.at[counter, '№'] = counter
-                        df.at[counter, 'Источник'] = source
-                        df.at[counter, 'Дата публикации'] = formatted_date
-                        df.at[counter, 'CVE'] = str_cve
-                        df.at[counter, 'Ссылки'] = link
-
-                        counter += 1
-                        print(f'На старице {link} найдены следующие CVE: {str_cve}')
-                    else:
-                        print(f'CVE на странице {link} не найдены.')
-
+        result_str = url.replace("www.", "")
+        match = re.search(r'//(.+?)/', result_str)
+        if match:
+            source = match.group(1)
         else:
-            print(f'Ошибка: {url} не содержит "pubdate".')
+            source = 'NEWS'
+        if is_valid_url(url):
+            driver.get(url)
+            time.sleep(2)
+            try:
+                feed = feedparser.parse(url)
+                if feed.entries:
+                    for entry in feed.entries:
+                        pub_date = entry.published
+                        try:
+                            date_object = datetime.strptime(pub_date, '%a, %d %b %Y %H:%M:%S %z').date()
+                        except ValueError:
+                            date_object = datetime.strptime(pub_date, '%a, %d %b %Y %H:%M:%S %Z').date()
 
-    functions.do_excel(source, df)
+                        formatted_date = date_object.strftime('%d.%m.%Y')
 
+                        if date1 <= date_object:
+
+                            link = entry.link
+                            title = entry.title
+
+                            driver.get(link)
+                            soup = functions.get_soup(driver, link, None)
+
+                            cve_pattern = re.compile(r'CVE-\d{4}-\d{4,7}')
+                            cve_matches = soup.find_all(string=cve_pattern)
+
+                            if cve_matches:
+                                for match in cve_matches:
+                                    cve = re.findall(cve_pattern, match)
+                                    str_cve = ' '.join(set(cve))
+
+                                df.at[counter, '№'] = counter + 1
+                                df.at[counter, 'Источник'] = source
+                                df.at[counter, 'Дата публикации'] = formatted_date
+                                df.at[counter, 'CVE'] = str_cve
+                                df.at[counter, 'Заголовок'] = title
+                                df.at[counter, 'Ссылки'] = link
+
+                                counter += 1
+                                output_info = f'{formatted_date}: {str_cve}: {link}'
+                                output_info = output_info.replace('\n', '').replace('\t', '')
+                                print(output_info)
+                            else:
+                                output_info = f'{formatted_date}: CVE Отсутствует: {link}'
+                                output_info = output_info.replace('\n', '').replace('\t', '')
+                                print(output_info)
+                else:
+                    print(f'{url} не является RSS-лентой.')
+            except Exception as e:
+                print(f'Ошибка при разборе страницы: {e}')
+        else:
+            print(f'Пожалуйста, убедитесь, что {url} валиден.')
+
+    functions.do_excel('NEWS', df)
     driver.close()
 
     input()
